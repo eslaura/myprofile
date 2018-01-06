f6f4299c6a51f9663052d77abfed00dbbf376a67
laura hallam <laura_hallam@yahoo.co.uk>
2018-01-06 12:36:19 +0100

    adding the pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,9 @@ class PagesController < ApplicationController
   end
 
   def about
+
+  end
+
+  def home
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/contact'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
 
-  get 'pages/about'
+  root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
